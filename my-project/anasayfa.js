2e84bf18f2045c3ba0fb5780d8ab7479e9be183d
OzgunDenizSevilmis <120256561+OzgunDenizSevilmis@users.noreply.github.com>
2025-03-26 03:04:26 +0300

    Giriş ve Ana Ekranın taslağı oluşturuldu.

---
+++ my-project/anasayfa.js
@@ -0,0 +1,4 @@
+function generateSuggestion() {
+  const suggestion = "Siyah slim-fit pantolon, beyaz pamuklu tişört ve açık renk bomber ceket öneriyoruz.";
+  document.getElementById("suggestionResult").innerText = suggestion;
+}
